f77ed15183dae12d2955d4e055a60324448eb269
acmahaja <acmahaja@gmail.com>
2021-02-23 14:41:00 +0200

    Added reviews

--- YelpCamp/index.js
+++ YelpCamp/index.js
@@ -2,15 +2,17 @@ const express = require('express');
 const path = require('path')
 const mongoose = require('mongoose');
 const methodOverride = require('method-override')
-const Campground = require('./models/campground')
 const ejsMate = require('ejs-mate')
 const app = express();
 const catchAsync = require('./utils/catchAsync')
 const ExpressError = require('./utils/ExpressError')
 const Joi = require('joi')
+const Campground = require('./models/campground')
+const Review = require('./models/review')
 
-const { campgroudSchema } = require('./schemas');
-
+const campgroudSchema = require('./schemas').campgroudSchema;
+const reviewSchema = require('./schemas').reviewSchema;
+const campground = require('./models/campground');
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
@@ -25,7 +27,17 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true,
 
 const validateCampground = (req, res, next) => {
     const { error } = campgroudSchema.validate(req.body.campground);
-    //const { error } = campgroudSchema.validate(req.body.campground);
+    if (error) {
+        const message = error.details.map(el => el.message).join(',')
+        throw new ExpressError(message, 400)
+    } else {
+        next();
+    }
+}
+
+const validateReview = (req, res, next) => {
+    console.log(req.body);
+    const { error } = reviewSchema.validate(req.body.review);
     if (error) {
         const message = error.details.map(el => el.message).join(',')
         throw new ExpressError(message, 400)
@@ -47,6 +59,23 @@ app.put('/campgrounds/:id', validateCampground, catchAsync(async(req, res) => {
 
 }))
 
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async(req, res) => {
+    await Campground.findByIdAndUpdate(req.params.id, { $pull: { reviews: req.params.reviewId } })
+    await Review.findById(req.params.reviewId);
+    res.redirect(`/campgrounds/${req.params.id}`);
+
+}))
+
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async(req, res) => {
+    // res.send('YOU MADE IT')
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review)
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${req.params.id}`);
+
+}))
 
 app.delete('/campgrounds/:id', async(req, res) => {
     await Campground.findByIdAndDelete(req.params.id);
@@ -55,13 +84,12 @@ app.delete('/campgrounds/:id', async(req, res) => {
 
 app.get('/campgrounds/:id/edit', async(req, res) => {
     const camp = await Campground.findById(req.params.id);
-    console.log(camp);
     res.render('campgrounds/edit', { camp })
-
 })
 
 app.get('/campgrounds/:id', catchAsync(async(req, res) => {
-    const camp = await Campground.findById(req.params.id);
+    const camp = await Campground.findById(req.params.id).populate('reviews');
+    console.log(camp.reviews);
     res.render('campgrounds/show', { camp });
 
 }))
@@ -73,8 +101,6 @@ app.get('/campgrounds', async(req, res) => {
 })
 
 app.post('/campgrounds', validateCampground, catchAsync(async(req, res) => {
-    //if (!req.body.camp) throw new ExpressError('Invalid Campground Data', 400);
-
     const camp = new Campground(req.body.campground)
     await camp.save();
     res.redirect(`/campgrounds/${camp.id}`);
--- YelpCamp/schemas.js
+++ YelpCamp/schemas.js
@@ -7,3 +7,8 @@ module.exports.campgroudSchema = Joi.object({
     image: Joi.string().required(),
     description: Joi.string().required(),
 }).required()
+
+module.exports.reviewSchema = Joi.object({
+    rating: Joi.number().required().min(1).max(5),
+    body: Joi.string().required()
+}).required()
