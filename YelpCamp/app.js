d75cf2651477ba3593dc6f4f31b2be5d41489b3b
Anjaney C Mahajan <acmahaja@gmail.com>
2022-01-10 10:59:13 +0200

    Added simple campground collection

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,16 +1,35 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const path = require('path')
+const path = require("path");
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database Connected");
+});
+
+const Campground = require("./models/campground");
 
 
 
-app.get('/', (req,res)=>{
-    res.render('home')
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-app.listen(3000, ()=>{
-    console.log('Serving port 3000');
-})
+app.get("/makecampground", async (req, res) => {
+  const camp = new Campground({
+    title: "My Backyard",
+  });
+  await camp.save();
+  res.send(camp);
+});
+
+app.listen(3000, () => {
+  console.log("Serving port 3000");
+});
